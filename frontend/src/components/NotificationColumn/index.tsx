39c841edaa3bedf21ffac7c6b34b6d272201c482
YeyeLeaf <ling930718@gmail.com>
2024-04-10 16:20:11 +0800

    add: notification page

--- frontend/src/components/NotificationColumn/index.tsx
+++ frontend/src/components/NotificationColumn/index.tsx
@@ -1,17 +1,34 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
-export default function NotificationColumn(): ReactElement {
-  return <div>
+import './index.scss';
+
+import Notification from "components/Notification";
+
+type propsType = Readonly<{
+
+  seeAllBtn: boolean;
+
+}>;
+
+export default function NotificationColumn(props: propsType): ReactElement {
+  const {
+    seeAllBtn
+  } = props;
+  return <div id="notificationColumn">
     <div className="header">
       <p>新通知</p>
-      <p>全部標示為已讀</p>
+      <button>全部標示為已讀</button>
+      {props.seeAllBtn === true ? <Link to="/notification" className="seeall">查看全部</Link> : <></>}
     </div>
     <div>
-
+      <Notification />
+      <Notification />
+      <Notification />
     </div>
-    <p>過去的通知</p>
+    <p className="header">過去的通知</p>
     <div>
-
+      <Notification />
     </div>
   </div>
 }
