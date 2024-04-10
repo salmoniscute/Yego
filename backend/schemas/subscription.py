816b45a3584328814bca35abf6a4f2a41c1653da
NT1026 <nelson20020222@gmail.com>
2024-04-10 14:55:25 +0800

    Subscription, Notification add new column 'type'

--- backend/schemas/subscription.py
+++ backend/schemas/subscription.py
@@ -1,6 +1,20 @@
 from pydantic import BaseModel
 
 
+class SubscriptionCreate(BaseModel):
+    type: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                { 
+                    "type": "website_bulletin"
+                }
+            ]
+        }
+    }
+
 class SubscriptionRead(BaseModel):
     uid: str
     component_id: str
+    type: str
