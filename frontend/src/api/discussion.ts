e5b32028c56d58a511907d05f47b662b931b5170
salmoniscute <noralin249@gmail.com>
2024-04-12 18:41:52 +0800

    edit course bulletin option button

--- frontend/src/api/discussion.ts
+++ frontend/src/api/discussion.ts
@@ -10,18 +10,21 @@ export async function getDiscussionList(): Promise<Array<Discussion>>{
     const result = [
         {
             discussion_id: "1",
+            uid:"F74106050",
             course_id: "",
             title:"第一周作業討論",
             discription: "討論HW1-1、1-2"
         },
         {
             discussion_id: "2",
+            uid:"F74106050",
             course_id: "",
             title:"第二周作業討論",
             discription: "討論HW2"
         },
         {
             discussion_id: "3",
+            uid:"F74106050",
             course_id: "",
             title:"第一次小考討論",
             discription: "討論2/29課堂小考"
@@ -33,11 +36,9 @@ export async function getDiscussionList(): Promise<Array<Discussion>>{
 export async function postDiscussion(uid:string , course_id:string , title:string , content:string): Promise<Discussion>{
     let url = "http://localhost:8080/api/discussion?uid="+uid+"&course_id="+course_id;
     let discussion ;
-    // 目前先自己設定id
     try {
         const response = await axios.post(url,{
             "title": title,
-            "id":"1",
             "release_time": "2021-09-01T00:00:00",
             "content": content,
           });
@@ -54,12 +55,14 @@ export async function postDiscussion(uid:string , course_id:string , title:strin
 export async function getDiscussionTopicList() : Promise<Array<DiscussionTopicInfo>>{
     const result = [
         {
+            uid:"F74106050",
             discussion_topic_id: "",
             discussion_id: "",
             release_time: 1703390840,
             title:"1-1第一題題意",
         },
         {
+            uid:"F74106050",
             discussion_topic_id: "",
             discussion_id: "",
             release_time: 1703390840,
@@ -72,7 +75,8 @@ export async function getDiscussionTopicList() : Promise<Array<DiscussionTopicIn
 
 export async function getDiscussionTopicContent(data: DiscussionTopicInfo): Promise<DiscussionTopicContent | null> {
     return Object.assign(data, {
-        publisher_id: "F74106050",
+        uid: "F74106050",
+        publisher:"林志芸",
         content:"新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財新年快樂恭喜發財",
     });
 };
@@ -81,13 +85,15 @@ export async function getDiscussionTopicReplyList() : Promise<Array<DiscussionTo
     const result = [
         {
             discussion_topic_reply_id:"",
-            publisher_id:"F74106050",
+            uid:"F74106050",
+            publisher:"林志芸",
             release_time: 1703390840,
             content:"新年快樂恭喜發財",
         },
         {
             discussion_topic_reply_id:"",
-            publisher_id:"F74106050",
+            uid:"F74106050",
+            publisher:"林志芸",
             release_time: 1703390840,
             content:"新年快樂恭喜發財",
         },
